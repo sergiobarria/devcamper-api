50899349adb9fdab5fab8b0e1fa2806c36534647
Sergio Barria <sergio.barria9210@gmail.com>
2021-07-13 00:04:15 -0500

    add basic routes and controllers for bootcamps

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// Import routes
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+// app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 app.listen(
