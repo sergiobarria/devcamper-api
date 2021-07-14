0353326a06bfad8cb0c179d9bc8739197e4fbac4
Sergio Barria <sergio.barria9210@gmail.com>
2021-07-13 23:38:29 -0500

    connect to mongodb and add crud operations

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./config/db');
 
 // Import routes
 const bootcamps = require('./routes/bootcamps');
@@ -8,7 +11,13 @@ const bootcamps = require('./routes/bootcamps');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
 const app = express();
+
+// Req body parser
+app.use(express.json());
 
 // Middleware
 if (process.env.NODE_ENV === 'development') {
@@ -18,10 +27,17 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-// app.use(express.json());
-
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
